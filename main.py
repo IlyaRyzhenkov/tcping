e46fa9562b84aff6abe46683a0d09376a1068a91
IlyaRyzhenkov <ilya.rigenkov2@gmail.com>
2019-11-13 12:48:06 +0500

    syn is works

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import sys
 
 if __name__ == "__main__":
     source_ip = socket.gethostbyname(socket.gethostname())
-    source_ip = "10.10.10.2"
     source_port = int(sys.argv[1])
     dest_ip = sys.argv[2]
     dest_port = int(sys.argv[3])
     creator = Creator.HeaderCreator(source_ip, dest_ip, source_port, dest_port, 0)
     packet = creator.make_SYN_quarry()
-
-    HOST = socket.gethostbyname(socket.gethostname())
-    # create a raw socket and bind it to the public interface
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-    s.bind((HOST, source_port))
+	
+    tcp = socket.getprotobyname("tcp")
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, tcp)
 
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     s.sendto(packet, (dest_ip, dest_port))
+    data = s.recvfrom(1024)
+    print(data)
